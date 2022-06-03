a68adefae4da7797029dcd40db316bc9165aa409
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-06-03 22:32:06 +0200

    1

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Przypisy stałych
-const btn = document.querySelector("#btn");
-const table = document.querySelector("main table");
-const blank = document.querySelector(".blank");
-const [ax, bx] = document.querySelectorAll(".ox");
-const [ay, by] = document.querySelectorAll(".oy");
+const btn = document.querySelector('#btn');
+const table = document.querySelector('main table');
+const blank = document.querySelector('.blank');
+const [ax, bx] = document.querySelectorAll('.ox');
+const [ay, by] = document.querySelectorAll('.oy');
 
 // Liczenie azymutu
 function getA(deltaX, deltaY) {
@@ -20,9 +20,13 @@ function getA(deltaX, deltaY) {
   } else {
     // Normalny wzór
     const phi = deltaX < 0 ? 200 : deltaY < 0 ? 400 : 0; // Czwartak
-    const arctg = math.unit(math.atan(deltaY / deltaX), "rad").toNumber("grad"); // ARCTG
+    const arctg = math.unit(math.atan(deltaY / deltaX), 'rad').toNumber('grad'); // ARCTG
     return arctg + phi;
   }
+}
+
+function getD(deltaX, deltaY) {
+  return Math.sqrt(deltaX ** 2 + deltaY ** 2);
 }
 
 function updateValues() {
@@ -33,24 +37,28 @@ function updateValues() {
     [bx.name]: Number(bx.value),
     [by.name]: Number(by.value),
   };
+
+  if (!pos.ax || !pos.ay || !pos.bx || !pos.by) return 0; // Jeżeli nie ma pełnych danych nie wykona się obliczenie
+
   [ax, ay, bx, by].forEach((el) => {
     // Czyszczenie pól po obliczeniach
-    el.value = "";
+    el.value = '';
   });
 
   const [deltaX, deltaY] = [pos.bx - pos.ax, pos.by - pos.ay]; // Liczenie przyrostów
 
-  const A = getA(deltaX, deltaY); // Oblcizenie azymutu (Widok na stronie)
+  const A = getA(deltaX, deltaY); // Obliczenie azymutu (Widok na stronie)
+  const D = getD(deltaX, deltaY); // Obliczenie długości (Widok na stronie)
 
-  const row = document.createElement("tr"); // Tworzenie linijki
-  const values = [pos.ax, pos.ay, pos.bx, pos.by, A.toFixed(4)];
-  for (let i = 0; i < 5; i++) {
-    // Tworzenie 5 komórek, początek pętli
-    const td = document.createElement("td");
+  const row = document.createElement('tr'); // Tworzenie linijki
+  const values = [pos.ax, pos.ay, pos.bx, pos.by, A.toFixed(4), D.toFixed(4)];
+  for (let i = 0; i < values.length; i++) {
+    // Tworzenie 6 komórek, początek pętli
+    const td = document.createElement('td');
     td.innerText = values[i];
     row.append(td);
   } // koniec pętli
   blank.remove(); // Usuwanie napisu w tabelce
   table.append(row); // Wypisanie linijki na stronie
 }
-btn.addEventListener("click", updateValues);
+btn.addEventListener('click', updateValues);
