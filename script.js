70c4058d8ff1364ebe0fd6e8eb909340815abfc4
JuliaKowalczyk <106777266+JuliaKowalczyk@users.noreply.github.com>
2022-06-03 11:05:08 +0200

    1

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const btn = document.querySelector('#btn');
-const table = document.querySelector('main table');
-const blank = document.querySelector('.blank');
-console.log(table, blank);
+// Przypisy stałych
+const btn = document.querySelector("#btn");
+const table = document.querySelector("main table");
+const blank = document.querySelector(".blank");
+const [ax, bx] = document.querySelectorAll(".ox");
+const [ay, by] = document.querySelectorAll(".oy");
 
-const [ax, bx] = document.querySelectorAll('.ox');
-const [ay, by] = document.querySelectorAll('.oy');
-
+// Liczenie azymutu
 function getA(deltaX, deltaY) {
   if (deltaX === 0 || deltaY === 0) {
+    // Szczególne przypadki
     if (deltaX === deltaY) {
       return 0;
     }
@@ -17,39 +18,39 @@ function getA(deltaX, deltaY) {
       return deltaX > 0 ? 0 : 200;
     }
   } else {
-    const phi = deltaX < 0 ? 200 : deltaY < 0 ? 400 : 0;
-    console.log('phi', phi);
-    const arctg = math.unit(math.atan(deltaY / deltaX), 'rad').toNumber('grad');
-    console.log('arctg', arctg);
+    // Normalny wzór
+    const phi = deltaX < 0 ? 200 : deltaY < 0 ? 400 : 0; // Czwartak
+    const arctg = math.unit(math.atan(deltaY / deltaX), "rad").toNumber("grad"); // ARCTG
     return arctg + phi;
   }
 }
 
 function updateValues() {
   const pos = {
+    // Wpisanie liczb
     [ax.name]: Number(ax.value),
     [ay.name]: Number(ay.value),
     [bx.name]: Number(bx.value),
     [by.name]: Number(by.value),
   };
   [ax, ay, bx, by].forEach((el) => {
-    el.value = '';
+    // Czyszczenie pól po obliczeniach
+    el.value = "";
   });
-  const [deltaX, deltaY] = [pos.bx - pos.ax, pos.by - pos.ay];
-  const A = getA(deltaX, deltaY);
 
-  console.log(pos);
-  console.log('Delta X:', deltaX, 'Delta Y:', deltaY);
-  console.log(A.toFixed(4));
+  const [deltaX, deltaY] = [pos.bx - pos.ax, pos.by - pos.ay]; // Liczenie przyrostów
 
-  const row = document.createElement('tr');
+  const A = getA(deltaX, deltaY); // Oblcizenie azymutu (Widok na stronie)
+
+  const row = document.createElement("tr"); // Tworzenie linijki
   const values = [pos.ax, pos.ay, pos.bx, pos.by, A.toFixed(4)];
   for (let i = 0; i < 5; i++) {
-    const td = document.createElement('td');
+    // Tworzenie 5 komórek, początek pętli
+    const td = document.createElement("td");
     td.innerText = values[i];
     row.append(td);
-  }
-  blank.remove();
-  table.append(row);
+  } // koniec pętli
+  blank.remove(); // Usuwanie napisu w tabelce
+  table.append(row); // Wypisanie linijki na stronie
 }
-btn.addEventListener('click', updateValues);
+btn.addEventListener("click", updateValues);
