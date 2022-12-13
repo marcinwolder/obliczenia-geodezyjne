3603e04bd92cee7b9a18e15078d32c6a54028fe7
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-12-13 22:16:14 +0100

    numbers to fixed 2 nums

--- src/components/Hausbrandt/HausbrandtData.tsx
+++ src/components/Hausbrandt/HausbrandtData.tsx
@@ -6,18 +6,23 @@ interface compProps {
   data: HausbrandtFuncs;
 }
 
+const checkFalseValue = (value: string | number)=>{
+  if(isNaN(Number(value))) return '0.00';
+  else return Number(value.toString()).toFixed(2);
+}
+
 export function HausbrandtData({data}:compProps):JSX.Element {
   return (
     <Box className='rounded-md shadow w-full' sx={{bgcolor: 'background.paper' }}>
       <List >
         <ListItem>
-          <Typography variant='body2'>f<sub>1</sub> : <b>{data.f1 || 0}</b></Typography>
+          <Typography variant='body2'>f<sub>1</sub> : <b>{checkFalseValue(data.f1)}</b></Typography>
         </ListItem>
         <ListItem>
-          <Typography variant='body2'>f<sub>2</sub> : <b>{data.f2 || 0}</b></Typography>
+          <Typography variant='body2'>f<sub>2</sub> : <b>{checkFalseValue(data.f2)}</b></Typography>
         </ListItem>
         <ListItem>
-          <Typography variant='body2'>f<sub>0</sub> : <b>{data.f0 || 0}</b></Typography>
+          <Typography variant='body2'>f<sub>0</sub> : <b>{checkFalseValue(data.f0)}</b></Typography>
         </ListItem>
           <Divider/>
         <ListItem>
@@ -26,16 +31,16 @@ export function HausbrandtData({data}:compProps):JSX.Element {
         <ListItem disablePadding>
           <List>
             <ListItem>
-              <Typography variant='body2'>f<sub>(1)</sub> : <b>{data.simple.lower[1] || 0}</b></Typography>
+              <Typography variant='body2'>f<sub>(1)</sub> : <b>{checkFalseValue(data.simple.lower[1])}</b></Typography>
             </ListItem>
             <ListItem>
-              <Typography variant='body2'>f<sub>(2)</sub> : <b>{data.simple.lower[2] || 0}</b></Typography>
+              <Typography variant='body2'>f<sub>(2)</sub> : <b>{checkFalseValue(data.simple.lower[2])}</b></Typography>
             </ListItem>
             <ListItem>
-              <Typography variant='body2'>f<sup>(1)</sup> : <b>{data.simple.upper[1] || 0}</b></Typography>
+              <Typography variant='body2'>f<sup>(1)</sup> : <b>{checkFalseValue(data.simple.upper[1])}</b></Typography>
             </ListItem>
             <ListItem>
-              <Typography variant='body2'>f<sup>(2)</sup> : <b>{data.simple.upper[2] || 0}</b></Typography>
+              <Typography variant='body2'>f<sup>(2)</sup> : <b>{checkFalseValue(data.simple.upper[2])}</b></Typography>
             </ListItem>
           </List>
         </ListItem>
@@ -44,18 +49,18 @@ export function HausbrandtData({data}:compProps):JSX.Element {
           <Typography variant='subtitle1'>Funkcje względnie kwadratowe:</Typography>
         </ListItem>
         <ListItem disablePadding>
-        <List>
+          <List>
             <ListItem>
-              <Typography variant='body2'>f<sub>[1]</sub> : <b>{data.quadratic.lower[1] || 0}</b></Typography>
+              <Typography variant='body2'>f<sub>(1)</sub> : <b>{checkFalseValue(data.quadratic.lower[1])}</b></Typography>
             </ListItem>
             <ListItem>
-              <Typography variant='body2'>f<sub>[2]</sub> : <b>{data.quadratic.lower[2] || 0}</b></Typography>
+              <Typography variant='body2'>f<sub>(2)</sub> : <b>{checkFalseValue(data.quadratic.lower[2])}</b></Typography>
             </ListItem>
             <ListItem>
-              <Typography variant='body2'>f<sup>[1]</sup> : <b>{data.quadratic.upper[1] || 0}</b></Typography>
+              <Typography variant='body2'>f<sup>(1)</sup> : <b>{checkFalseValue(data.quadratic.upper[1])}</b></Typography>
             </ListItem>
             <ListItem>
-              <Typography variant='body2'>f<sup>[2]</sup> : <b>{data.quadratic.upper[2] || 0}</b></Typography>
+              <Typography variant='body2'>f<sup>(2)</sup> : <b>{checkFalseValue(data.quadratic.upper[2])}</b></Typography>
             </ListItem>
           </List>
         </ListItem>
