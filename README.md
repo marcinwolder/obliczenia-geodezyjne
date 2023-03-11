58b9940bc1fb2c0e2b5e5789ee851c67260c069b
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-03-11 18:51:51 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
+<img src="./public/logo.png" width="400">
+
+# Project for geodetic calculations
+
+Project for geodetic calculations. Includes calculators for the most commonly used surveying operations
+
+<br/>
+
+---
+
+## Tech Stack
+
+**Client:** React, Tailwind, MaterialUI
+
+<br/>
+
+---
+
+## Demo
+
+<div style="display: flex; align-items: start; padding-top: .5em">
+<img src="./public/screen1.png" >
+<img src="./public/screen2.png" >
+<img src="./public/screen3.png" >
+</div>
+
+#### Live DEMO:
+
 https://obliczenia-geodezyjne.netlify.app/
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
-const _Header = ():JSX.Element=>{  
-  return (
-    <>
-      <div className="sticky -top-20 mx-auto flex-col w-80 bg-red-salsa p-3 mt-4 rounded-lg z-10">
-        <p className="text-3xl font-bold">Program </p>
-        <p className="text-xl">Do opracowań geodezyjnych </p>
-        <p className="mt-4 text-m font-semibold">Julia Kowalczyk</p>
-      </div>
-      <div className="w-80 mx-auto relative -z-10">
-          <div className="bg-red-salsa w-6 h-8 relative -top-2 rounded-br-full"></div>
-          <div className="bg-seashell w-6 h-6 absolute top-0 rounded-tl-full"></div>
-      </div>
-    </>
-  )
+const _Header = (): JSX.Element => {
+	return (
+		<>
+			<div className='sticky -top-20 mx-auto flex-col w-80 bg-red-salsa p-3 mt-4 rounded-lg z-10'>
+				<p className='text-3xl font-bold'>Program </p>
+				<p className='text-xl'>Do opracowań geodezyjnych </p>
+				<p className='mt-4 text-m font-semibold'>
+					Marcin Wolder & Julia Kowalczyk
+				</p>
+			</div>
+			<div className='w-80 mx-auto relative -z-10'>
+				<div className='bg-red-salsa w-6 h-8 relative -top-2 rounded-br-full'></div>
+				<div className='bg-seashell w-6 h-6 absolute top-0 rounded-tl-full'></div>
+			</div>
+		</>
+	);
 };
 
 export const Header = _Header;
